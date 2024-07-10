a8ce17593295d0379911f48c848de718e9532415
sattamap <sattamap@gmail.com>
2024-07-10 23:13:08 +0600

    Setup a basic router with integrating react-router-dom and added a Home.tsx

---
+++ src/router/index.tsx
@@ -0,0 +1,14 @@
+import Home from "@/pages/Home/Home";
+import { createBrowserRouter } from "react-router-dom";
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+
+  },
+]);
+
+export default router;
